1155e313570f748a635c93c1d922c4435080eb2d
Marcos Barbosa <94478379+Marcos-exe@users.noreply.github.com>
2021-12-17 10:27:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function titulo(){
+    let input = document.getElementById("h1")
+    let h1 = document.getElementsByTagName("h1")[0];
+    h1.innerHTML = input.value;
+}
+function paragrafo(){
+    let input2 = document.getElementById("paragrafo");
+    let p1 = document.getElementsByTagName("p")[0];
+    p1.innerHTML = input2.value;
+}
+
+function red(){
+    let h1 = document.getElementsByTagName("h1")[0];
+    let p1 = document.getElementsByTagName("p")[0];
+    h1.style.color = "red";
+    p1.style.color = "red";
+}
+
+function green(){
+    let h1 = document.getElementsByTagName("h1")[0];
+    let p1 = document.getElementsByTagName("p")[0];
+    h1.style.color = "green";
+    p1.style.color = "green";
+}
+
+function blue(){
+    let h1 = document.getElementsByTagName("h1")[0];
+    let p1 = document.getElementsByTagName("p")[0];
+    h1.style.color = "blue";
+    p1.style.color = "blue";
+}
+
+function yellow(){
+    let h1 = document.getElementsByTagName("h1")[0];
+    let p1 = document.getElementsByTagName("p")[0];
+    h1.style.color = "yellow";
+    p1.style.color = "yellow";
+}
+
+function pink(){
+    let h1 = document.getElementsByTagName("h1")[0];
+    let p1 = document.getElementsByTagName("p")[0];
+    h1.style.color = "pink";
+    p1.style.color = "pink";
+}
+
+function black(){
+    let h1 = document.getElementsByTagName("h1")[0];
+    let p1 = document.getElementsByTagName("p")[0];
+    h1.style.color = "black";
+    p1.style.color = "black";
+}
